2c7c49e979e09507204a81fa3556016f2a1dd240
TravisA <travis_aucoin@gemvision.com>
2017-10-31 22:05:42 -0500

    html

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,22 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="WrapHomeOnepage">
+        <header className="WrapHomeOnepageHeader">
+          <div className="WrapHomeOnepageHeader HomeOnepageRestyle">
+            <div className="HomeOnepageHeaderLogo">
+              <a href="index.html"><img alt="logo-deli" src="images/icons/logo.png" />></a>
+            </div>
+
+            <div className="HomeOnepageHeaderBtnShowMenu">
+              <div id="trigger-overlay" className="Hamburger HamburgerSqueeze">
+                <span className="HamburgerBox">
+                  <span className="HamburgerInner"></span>
+                </span>
+              </div>
+            </div>
+          </div>
+        </header>
         <h1>Users</h1>
         {this.state.users.map(user =>
           <div key={user.id}>{user.username}</div>
