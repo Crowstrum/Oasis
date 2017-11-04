1cd05024c2b72fd40060e7e87e6c0ea28e21e578
TravisA <travis_aucoin@gemvision.com>
2017-11-04 18:30:30 -0500

    fixed styling

--- client/src/Sections/Slider/Slider.js
+++ client/src/Sections/Slider/Slider.js
@@ -7,9 +7,20 @@ class Slider extends Component {
         
 		<section id="slider-deli">
 			<div className="rev_slider_wrapper for-vertical-page">
-				<div className="container" style={{minHeight: '700px', display:'flex',justifyContent:'center',alignItems:'center'}}>
-					<p style={{fontSize:'80px',fontFamily: 'Lobster, cursive', color: 'white'}}>Upgrade Your Smoke</p>
-				<video className="videoInsert" autoPlay loop>
+				<div className="container-fluid" style={{minHeight: '700px', display:'flex',justifyContent:'center',alignItems:'center'}}>
+					<div className="tabletOrHigherOnly">
+						<p style={{fontSize:'80px',fontFamily: 'Lobster, cursive', color: 'white'}}>Upgrade Your Smoke</p>
+					</div>
+					<div className="mobileOnlyLg">
+						<p style={{fontSize:'40px',fontFamily: 'Lobster, cursive', color: 'white'}}>Upgrade Your Smoke</p>
+					</div>
+					<div className="mobileOnlyMd">
+						<p style={{fontSize:'35px',fontFamily: 'Lobster, cursive', color: 'white'}}>Upgrade Your Smoke</p>
+					</div>
+					<div className="mobileOnlySm">
+						<p style={{fontSize:'32px',fontFamily: 'Lobster, cursive', color: 'white'}}>Upgrade Your Smoke</p>
+					</div>
+				<video className="videoInsert" autoPlay loop playsInline>
   					<source src={require("../../Video/smokeVideo.mp4")} type="video/mp4"/>
  					 Your browser does not support the video tag.
  				</video>
